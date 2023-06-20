7068c7583961841b66ec6d3013b20d22be4fe3c8
Tushar Singh <tusharsingh09022003@gmail.com>
2023-06-21 01:13:25 +0530

    sleep function

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 import { gsap } from "gsap"
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+  console.log("Hello");
+    sleep(4000).then(() => { 
+        gsap.to('svg',{
+            scale: 0
+    })
+    gsap.to(".blinder",{
+        scaleY: 0,
+        stagger: 0.1,
+        ease: 'power3.out'
+    })
+   });
 
-await new Promise(resolve => setTimeout(resolve, 4000));
-
-
-gsap.to('svg',{
-        scale: 0
-})
-gsap.to(".blinder",{
-    scaleY: 0,
-    stagger: 0.1,
-    ease: 'power3.out'
-})
